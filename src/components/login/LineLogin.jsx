4ea4dc54ee769a1c773894ed2ebed39aad8cca6d
Ro2a Almassoud <ro2a.almasoud@gmail.com>
2024-06-30 18:51:36 +0200

    redirect uri fix

--- src/components/login/LineLogin.jsx
+++ src/components/login/LineLogin.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import './LineLogin.css';
 import LineButtonImage from '../../assets/btn_login_base.png';
 function LineLogin() {
-  
   const [error, setError] = useState('');
   const [show, setShow] = useState(false);
 
@@ -11,6 +10,7 @@ function LineLogin() {
 
   const lineLogin = () => {
     const redirectUri = encodeURIComponent(location.origin);
+    console.log('redirectUri: ', redirectUri);
     window.location.href = `https://access.line.me/oauth2/v2.1/login?returnUri=%2Foauth2%2Fv2.1%2Fauthorize%2Fconsent%3Fresponse_type%3Dcode%26client_id%3D1661093729%26redirect_uri%3D${redirectUri}%26state%3DJqv_5Bdv5IKDahQqlqIeGtvXS6HKJMLMi_e3lmoLQAar_WbF_cRMkUXqyxMADZnqDojHezxqeyWQGsScdVxjsJK6C0HKVK4v-Q9Xnz0tpuaFcUVHiomhYph6GYfMt6Ojn7nQapbwMm1e0xLaRtfv44GqvfMeEq-5n8vV8hWWpaQ3hpmVwZ3ZgdB5fFR4kpNHusAqZ-fvIYc71zHbHcV1XTRW2Ig5952zUw_J_VsL7gc%26scope%3Dprofile%2520openid&loginChannelId=1661093729&loginState=u7tA4gvzKs2vkXFkeTzcKO#`;
   };
 
